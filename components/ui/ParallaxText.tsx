ffebe06851c15eb4cf611bb2cbac2d3734386158
paji1 <tahamaroc1@gmail.com>
2025-04-24 17:16:32 +0100

    feat: implement FlipWords component for animated text display in ParallaxText; update motion library to latest version

--- components/ui/ParallaxText.tsx
+++ components/ui/ParallaxText.tsx
@@ -4,34 +4,29 @@ import React from "react";
 import { GoDotFill } from "react-icons/go";
 import Marquee from "react-fast-marquee";
 import { RobotoMono } from "@/utils/fonts";
+import { FlipWords } from "./flip-words";
 
+const words = [
+	"Passionate",
+	"Full Stack",
+	"Web Developer",
+	"Software Engineer",
+	"Transforming Ideas",
+	"Into Digital Experiences",
+];
 const ParallaxText = () => {
 	return (
 		<div className={`${RobotoMono.className} card p-4 ml-2`}>
 			<Image src={bg1} alt="bg1" className="bg-card" />
-			<Marquee pauseOnHover autoFill speed={110}>
-				<div className="flex items-center gap-2 mr-2">
-					<p className="text-white/40 uppercase text-xs tracking-widest">
-						full stack{" "}
-						<span className="text-lightest-slate">developer</span>
-					</p>
-					<GoDotFill className="text-lightest-slate/60" />
-				</div>
-				<div className="flex items-center gap-2 mr-2">
-					<p className="text-white/40 uppercase text-xs tracking-widest">
-						full stack{" "}
-						<span className="text-lightest-slate">developer</span>
-					</p>
-					<GoDotFill className="text-lightest-slate/60" />
-				</div>
-				<div className="flex items-center gap-2 mr-2">
-					<p className="text-white/40 uppercase text-xs tracking-widest">
-						full stack{" "}
-						<span className="text-lightest-slate">developer</span>
-					</p>
-					<GoDotFill className="text-lightest-slate/60" />
-				</div>
-			</Marquee>
+			<div className="flex items-center gap-2 mr-2">
+				<GoDotFill className="text-lightest-slate/60" />
+				<p className="text-white/40 uppercase text-xs tracking-widest">
+					Hi there, I'm Taha
+				</p>
+				<p className="text-white/40 uppercase text-xs tracking-widest">
+					<FlipWords words={words} /> <br />
+				</p>
+			</div>
 		</div>
 	);
 };
